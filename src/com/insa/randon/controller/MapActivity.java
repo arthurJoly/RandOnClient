999850eb0a4c186d1d97bfded9cf5b5935320df0
arthurJoly <arthurjoly11@gmail.com>
2014-11-03 18:40:54 +0100

    show points that represent a hike on the map

--- src/com/insa/randon/controller/MapActivity.java
+++ src/com/insa/randon/controller/MapActivity.java
@@ -1,11 +1,18 @@
 package com.insa.randon.controller;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.insa.randon.R;
 import com.insa.randon.R.id;
 import com.insa.randon.R.layout;
@@ -21,7 +28,14 @@ public class MapActivity extends Activity  {
         MapFragment fm = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         googleMap = fm.getMap();
         googleMap.setMyLocationEnabled(true);
-
+        
+        //Create a new hike (a list of coordinate)
+        List<LatLng> newHike = new ArrayList<LatLng>();
+        newHike.add(new LatLng(45.781307, 4.873902));
+        newHike.add(new LatLng(45.783971, 4.880210));
+        newHike.add(new LatLng(45.785347, 4.872700));
+        newHike.add(new LatLng(45.783641, 4.864847));
+        this.showHike(newHike);
         
         /*
         LatLng myPosition;
@@ -40,5 +54,18 @@ public class MapActivity extends Activity  {
         	googleMap.animateCamera(yourLocation);
         }*/
     }
+	
+	protected void showHike(List<LatLng> hike){
+		BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.marker);
+				
+		int size = hike.size();
+		int i;
+		for(i=0;i<size;i++)
+		{
+			googleMap.addMarker(new MarkerOptions()
+	        .position(hike.get(i))
+	        .icon(icon));
+		}
+	}
 }
 
