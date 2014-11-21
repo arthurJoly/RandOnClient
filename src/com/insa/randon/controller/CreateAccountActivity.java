fca2cb302f0dac894f8020b4f0ecda569fbabf26
Pauline Montmayeur <pauline.montmayeur@insa-lyon.fr>
2014-11-21 18:40:21 +0100

    Set up code for account creation Changed UI component id in activity_create_acount layout

--- src/com/insa/randon/controller/CreateAccountActivity.java
+++ src/com/insa/randon/controller/CreateAccountActivity.java
@@ -8,48 +8,73 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.insa.randon.R;
+import com.insa.randon.services.UserServices;
+import com.insa.randon.utilities.ErrorCode;
+import com.insa.randon.utilities.TaskListener;
 
 public class CreateAccountActivity extends BaseActivity {
-		Button createAccountButton;
-		EditText editTextUserName;
-		EditText editTextPassword;
-		EditText editTextConfirmPassword;
-		EditText editTextEmail;
+	Button createAccountButton;
+	EditText editTextUserName;
+	EditText editTextPassword;
+	EditText editTextConfirmPassword;
+	EditText editTextEmail;
+
+	Context context;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_account);
+		context=this;
+
+		createAccountButton = (Button) findViewById(R.id.button_create_account);
+		editTextUserName = (EditText) findViewById(R.id.editText_create_user_name);
+		editTextPassword = (EditText) findViewById(R.id.editText_create_password);
+		editTextConfirmPassword = (EditText) findViewById(R.id.editText_create_confirm_password);
+		editTextEmail = (EditText) findViewById(R.id.editText_create_email);
+	}
+
+	public void onButtonClick(View view){
+		//Check if all field are completed
+
+		String email = editTextEmail.getText().toString();
+		String username = editTextUserName.getText().toString();
+		String password = editTextPassword.getText().toString();
+		String confirmPassword = editTextConfirmPassword.getText().toString();
+
+		boolean notEmpty = (email.equals("") && username.equals("") && password.equals("") && confirmPassword.equals(""));
+		boolean passwordConfirmed = password.equals(confirmPassword);
+		//TODO: check if email is valid that the field don't contain special characters
 		
-		Context context;
+		if (notEmpty){
+			Toast.makeText(context, R.string.empty_fields, Toast.LENGTH_SHORT).show();
+		} else if (!passwordConfirmed){
+			Toast.makeText(context, R.string.password_not_confirmed, Toast.LENGTH_SHORT).show();
+		} else {
 		
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.activity_create_account);
-			context=this;
-			
-			createAccountButton = (Button) findViewById(R.id.button_create_account);
-			editTextUserName = (EditText) findViewById(R.id.editText_user_name);
-			editTextPassword = (EditText) findViewById(R.id.editText_password);
-			editTextConfirmPassword = (EditText) findViewById(R.id.editText_confirm_password);
-			editTextEmail = (EditText) findViewById(R.id.editText_email);
-		}
-		
-		public void onButtonClick(View view){
-			//Check if all field are completed
-			//Call webservices
-			new AlertDialog.Builder(this)
-		    .setTitle("Compte créé")
-		    .setMessage("Votre compte a été créé avec succès!")
-		    .setCancelable(false)
-		    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int which) { 
-		        	Intent intent = new Intent(context, HomeActivity.class);
+			//initialize task listener
+			TaskListener accountCreationListener = new TaskListener() {
+
+				@Override
+				public void onSuccess(String content) {
+					Toast.makeText(context, R.string.account_creation_success, Toast.LENGTH_SHORT).show();	
+					Intent intent = new Intent(context, HomeActivity.class);
 					startActivity(intent);
 					finish();
-		        }
-		     })
-		    .setIcon(android.R.drawable.ic_dialog_alert)
-		    .show();
-			
+				}
+
+				@Override
+				public void onFailure(ErrorCode errCode) {
+					Toast.makeText(context, R.string.account_creation_failure, Toast.LENGTH_SHORT).show();					
+				}
+			};
+
+			UserServices.createUser(username, password, email, accountCreationListener);
 		}
+
+	}
 
 }
