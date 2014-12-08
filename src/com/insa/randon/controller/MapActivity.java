2012c624dd0035c44c1995c3ea0687f7d956d6cb
arthurJoly <arthurjoly11@gmail.com>
2014-12-08 21:48:23 +0100

    add timer, difference of height and speed to mapActivity

--- src/com/insa/randon/controller/MapActivity.java
+++ src/com/insa/randon/controller/MapActivity.java
@@ -11,6 +11,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.Button;
@@ -25,20 +26,55 @@ import com.insa.randon.model.Map;
 public class MapActivity extends BaseActivity {	
 	private static final int MIN_TIME_INTERVAL_MS = 1000;
 	private static final int MIN_DISTANCE_INTERVAL_M = 3;
+	private static final int DELAY = 60000;
 	private static final String DISTANCE_UNIT = " m";
-	
-	Context context;
+	private static final String HOUR_UNIT = " h";
+	private static final String MINUTE_UNIT = " min";
+	private static final String SPEED_UNIT = " km/h";
+	private static final float CONVERT_SPEED_UNIT_TO_KMH = (float) 0.06;
 	
 	private Map map;
 	private LocationManager locManager;
 	private Hike newHike;
 	private TextView distanceTextView;
 	private TextView speedTextView;
+	private TextView durationTextView;
+	private TextView altitudeTextView;
 	private Button finishHikeButton;
 	private FollowHikeLocationListener locListener;
 	private ViewStub mapContainer;
 	private AlertDialog alert = null;
 	
+	Context context;
+	long startTime = 0;
+	
+	//timer
+	Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+    	float previousDistance=-1;
+    	float speed=0;
+    	
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            int hours = minutes / 60;
+            seconds = seconds % 60;
+            minutes = minutes % 60;
+
+            durationTextView.setText(String.format("%d"+HOUR_UNIT+" %d"+MINUTE_UNIT, hours, minutes));
+            
+            //COMPUTE SPEED
+            if(previousDistance!=-1){
+                speed = (newHike.getDistance()-previousDistance)/DELAY;
+            }
+            speedTextView.setText(speed*CONVERT_SPEED_UNIT_TO_KMH+SPEED_UNIT);
+            previousDistance=newHike.getDistance();
+
+            timerHandler.postDelayed(this, DELAY);
+        }
+    };
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,9 +94,16 @@ public class MapActivity extends BaseActivity {
         
         distanceTextView = (TextView) findViewById(R.id.distance_textView);
         speedTextView = (TextView) findViewById(R.id.speed_textView);
-        
+        durationTextView = (TextView) findViewById(R.id.duration_textView);
+        altitudeTextView = (TextView) findViewById(R.id.altitude_textView);
+        altitudeTextView.setText("0"+DISTANCE_UNIT);
+       
         locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
         locListener = new FollowHikeLocationListener(); 
+        
+        //Start timer
+        startTime = System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable, 0);
         
         //check if GPS is enabled
         PackageManager pm = getPackageManager();
@@ -77,6 +120,8 @@ public class MapActivity extends BaseActivity {
         //Create a new hike
         //TODO : test if we are in creation mode or not
         newHike = new Hike();
+        
+        //HIKE TEST
         newHike.extendHike(new LatLng(45.781307, 4.873902));
         newHike.extendHike(new LatLng(45.783971, 4.880210));
         newHike.extendHike(new LatLng(45.785347, 4.872700));
@@ -121,19 +166,35 @@ public class MapActivity extends BaseActivity {
     
     
     public void onButtonClick(View view) {
+    	timerHandler.removeCallbacks(timerRunnable); //Stop timer
 		Intent intent = new Intent(context, FinishHikeActivity.class);
 		intent.putExtra("hike", newHike);
 		startActivity(intent);
     }
 	
+    //------------------ LOCATION LISTENER ------------------------------------
 	public class FollowHikeLocationListener implements LocationListener{
+		double currentAltitude=0;
+    	double previousAltitude=Double.NEGATIVE_INFINITY;
+    	double positiveHeightDifference=0;
+    	
 	    @Override
 	    public void onLocationChanged(Location location)
 	    {    
 	    	//TODO : test if we are in creation mode
+	    	//if we follow a hike that we downloaded maybe we can create a new hike like but we don't call followingHike
 	    	map.followingHike(new LatLng(location.getLatitude(),location.getLongitude()));
 	    	newHike.extendHike(new LatLng(location.getLatitude(),location.getLongitude()));
 	    	distanceTextView.setText(newHike.getDistance() + DISTANCE_UNIT);
+
+	    	//Compute the positive difference of height
+	    	currentAltitude = location.getAltitude();
+	    	if(currentAltitude>previousAltitude && previousAltitude!=Double.NEGATIVE_INFINITY)
+	    	{
+	    		positiveHeightDifference+=currentAltitude-previousAltitude;
+	    		altitudeTextView.setText(positiveHeightDifference+DISTANCE_UNIT);
+	    	}
+	    	previousAltitude=currentAltitude;
 	    }
 
 	    @Override
