24f7366c89e35f5063a7f88cb978e2a783219901
Pauline Montmayeur <pauline.montmayeur@insa-lyon.fr>
2015-01-08 23:18:19 +0100

    - removed download hike button in consulting hike view (just for demo)

--- src/com/insa/randon/controller/ConsultingHikeActivity.java
+++ src/com/insa/randon/controller/ConsultingHikeActivity.java
@@ -112,25 +112,5 @@ public class ConsultingHikeActivity extends BaseActivity {
 	        e.printStackTrace();
 	    }
 	}
-
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    // Inflate the menu items for use in the action bar
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.consulting_menu, menu);
-	    return super.onCreateOptionsMenu(menu);
-	}
-	
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case R.id.action_download_hike:
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
 
